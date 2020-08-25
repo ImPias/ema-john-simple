3680cfc537e8eda8d41ba4747e25dd85e2dc11be
impias <kazipias123@gmail.com>
2020-08-25 23:41:19 +0600

    Basic Ema-John After Module 34

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
+    const { img, name, seller, price, stock } = props.product;
     return (
-        <div>
-            <h1>{props.product.name}</h1>
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <br />
+                <p><small>by: {seller}</small></p>
+                <p>${price}</p>
+                <p><small>Only {stock} left in stock - order soon.</small></p>
+                <button onClick={() => props.handleAddProduct(props.product)} className="main-button"><FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
+            </div>
         </div>
     );
 };
